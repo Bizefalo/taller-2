a1c307e4ad48e5231a924ec9563ed944b7ace292
Bizefalo <164973530+Bizefalo@users.noreply.github.com>
2024-05-26 17:06:22 -0400

    Update Bodega.cpp

--- Bodega.cpp
+++ Bodega.cpp
@@ -1,14 +1,63 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <queue>
+#include <list>
 class Bodega {
 private:
     ListaProductos listaProductos;
 
 public:
     void cargarProductosDesdeArchivo(const std::string& nombreArchivo) {
-        // Implementar la lectura del archivo y cargar los productos
+        std::ifstream archivo(nombreArchivo);
+        if (!archivo.is_open()) {
+            std::cerr << "error al abri el archivo: " << nombreArchivo << std::endl;
+            return;
+        }
+
+        std::string linea;
+        while (std::getline(archivo, linea)) {
+            std::stringstream ss(linea);
+            std::string id, categoria, subCategoria, precioStr, cantidadStr;
+
+            std::getline(ss, id, ',');
+            std::getline(ss, categoria, ',');
+            std::getline(ss, subCategoria, ',');
+            std::getline(ss, precioStr, ',');
+            std::getline(ss, cantidadStr, ',');
+
+            double precio = std::stod(precioStr);
+            int cantidad = std::stoi(cantidadStr);
+
+            Producto* producto = new Producto(id, categoria, subCategoria, precio, cantidad);
+            listaProductos.agregarProducto(producto);
+        }
+
+        archivo.close();
     }
 
+    //-------------------------------------------------------------------------------------------------------------
+
     void guardarProductosEnArchivo(const std::string& nombreArchivo) {
-        // Implementar la escritura de los productos en el archivo
+        std::ofstream archivo(nombreArchivo);
+        if (!archivo.is_open()) {
+            std::cerr << "error al abrir el archivo: " << nombreArchivo << std::endl;
+            return;
+        }
+
+        NodoProducto* actual = listaProductos.cabeza;
+        while (actual != nullptr) {
+            Producto* producto = actual->producto;
+            archivo << producto->getId() << ","
+                    << producto->getCategoria() << ","
+                    << producto->getSubCategoria() << ","
+                    << producto->getPrecio() << ","
+                    << producto->getCantidad() << "\n";
+            actual = actual->siguiente;
+        }
+
+        archivo.close();
     }
 
     void agregarProducto(Producto* producto) {
